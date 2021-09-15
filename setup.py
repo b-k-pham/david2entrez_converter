5984771b42f57eab28c8d6499618b0d54c4725f4
b-k-pham <86892623+b-k-pham@users.noreply.github.com>
2021-09-15 03:26:40 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from distutils.core import setup
 setup(
-  name = 'david2entrez_converter',         # How you named your package folder (MyLib)
-  packages = ['david2entrez_converter'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Convert the HGNC gene names to ENTREZ id available in the DAVID bioinformatics database.',   # Give a short description about your library
-  author = 'Benjamin Pham',                   # Type in your name
-  author_email = 'bkpham@ucsd.edu',      # Type in your E-Mail
-  url = 'https://github.com/user/b-k-pham',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['DAVID','Gene','Conversion'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name = 'david_to_entrez_converter',
+  packages = ['david_to_entrez_converter'],
+  version = '0.1',
+  license='MIT',
+  description = 'Convert the HGNC gene names to ENTREZ id available in the DAVID bioinformatics database.',
+  author = 'Benjamin Pham',
+  author_email = 'bkpham@ucsd.edu',
+  url = 'https://github.com/user/b-k-pham',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['DAVID','Gene','Conversion'],
+  install_requires=[
           'pandas',
           'numpy',
           'os'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
